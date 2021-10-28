b3a62d801fb3e90948f06bd01a218078954afa3a
Kyle Trijo <kyle.trijo@gmail.com>
2021-10-28 20:17:27 +0800

    added help and about page. new logo and icons.

---
+++ php/database_query.php
@@ -0,0 +1,23 @@
+<?php
+    $host = "localhost";
+    $user = "X33958764";
+    $pwd = "X33958764";
+    $dbname = "X33958764";
+
+    $dbc = mysqli_connect($host, $user, $pwd, $dbname);
+
+    //Check connection
+    if (mysqli_connect_errno()) {
+            echo "Failed to connect to database."
+                    . mysqli_connect_error()
+                    . "<br> Error number: "
+                    . mysqli_connect_errno();
+    }
+
+    else
+    {
+        echo("I connected.. YAYYYYYYY");
+    }
+
+?>
+
